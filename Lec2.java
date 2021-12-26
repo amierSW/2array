ab342f12bb7382ab7ecdd23ef31e1211ed5e8bd3
amierSW <96127668+amierSW@users.noreply.github.com>
2021-12-26 16:29:29 +0200

    Add files via upload

---
+++ Lec2.java
@@ -0,0 +1,41 @@
+
+package lec2;
+
+import java.util.Scanner;
+
+
+public class Lec2 {
+
+
+    public static void main(String[] args) {
+       
+        Scanner a = new Scanner (System.in);
+        
+        
+    int b;
+     String c ;
+     try {
+          System.out.println("ادخل اسم المستخدم");
+          c = a.nextLine();
+         System.out.println( "ادخل رقم المرور");   
+           b = a.nextInt();
+           
+  
+         
+     }
+         
+         
+       catch(Exception A) {
+           System.out.println("يوجد خطأ في اسم المستخدم او رقم المرور حاول مرة أخرى");
+         
+         
+       }
+         
+     
+         
+     // By amir
+        
+     }    
+    }
+    
+
